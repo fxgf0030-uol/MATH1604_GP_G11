f3e8874d999465ce312c3aa337ff2c618929f81f
fxgf0030-uol <fxgf0030@leeds.ac.uk>
2026-04-01 20:45:59 +0100

    Initial implementation of function 2 (M3)

--- scripts/data_analysis_M3.py
+++ scripts/data_analysis_M3.py
@@ -31,5 +31,39 @@ def generate_means_sequence(collated_answers_path):
         means.append(sum(question_answers)/len(question_answers))
 
     return means
+
+    
 # Test
 print(generate_means_sequence("mock/M_output/collated.txt"))
+
+
+def visualize_data(collated_answers_path, n):
+    
+    means = generate_means_sequence(collated_answers_path)
+
+    with open(collated_answers_path, "r") as file:
+        lines = file.readlines()
+
+    smaller_groups = []
+    for index in range(0, len(lines), 101):
+        individual_group = []
+        for line in lines[index: index+100]:
+            if int(line.rstrip()) != 0:
+                individual_group.append(int(line.rstrip()))
+            else:
+                pass
+        smaller_groups.append(individual_group)
+
+    if n == 1:
+        plt.scatter(range(1, 101), means)
+        plt.xlabel("Question Number")
+        plt.ylabel("Answer Mean")
+        plt.show()
+    elif n == 2:
+        for answer in smaller_groups:
+            plt.plot(range(1, 101), answer, alpha=0.5)
+        plt.xlabel("Question Number")
+        plt.ylabel("Answer Value")
+        plt.show()
+    else:
+        print("Wrong n input; should be 1 or 2")
