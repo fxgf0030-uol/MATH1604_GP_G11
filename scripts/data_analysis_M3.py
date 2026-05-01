ba81b34153afc9178e079c3bafb716c9805c68cf
fxgf0030-uol <fxgf0030@leeds.ac.uk>
2026-05-01 13:16:02 +0100

    added extract_answers inside M3 functions to extract integer data from collated_answers.txt proper format for analysis use

--- scripts/data_analysis_M3.py
+++ scripts/data_analysis_M3.py
@@ -5,22 +5,25 @@ def generate_means_sequence(collated_answers_path):
     def extract_answers(participant):
 
         answers = []
+        # iterate through all elements of this list of lines of one participant
         for index, line in enumerate(participant):
 
+            # define a question group for each question
             if "Question " in line:
-
+                
                 question_group = participant[index+1: index+5]
                 checker_iteration = 0
-
+                # iterate through answers of that question group
                 for smaller_index, answer in enumerate(question_group):
-                    
+                    # identify which answer is taken
                     if "[X]" in answer:
                         answer_number = smaller_index + 1
                         answers.append(answer_number)
                         break
                     else:
+                        # if an answer doesn't contain [X] then increase check count by 1
                         checker_iteration += 1
-                    
+                # if check count was made for all answers then no answer was given therefore 0
                 if checker_iteration >= 4:
                     answers.append(0)
             else:
@@ -83,9 +86,40 @@ def generate_means_sequence(collated_answers_path):
 
 
 def visualize_data(collated_answers_path, n):
-    
-    means = generate_means_sequence(collated_answers_path)
 
+    # One participant data from the collated answers file transformed into a list of lines
+    def extract_answers(participant):
+
+        answers = []
+        # iterate through all elements of this list of lines of one participant
+        for index, line in enumerate(participant):
+
+            # define a question group for each question
+            if "Question " in line:
+                
+                question_group = participant[index+1: index+5]
+                checker_iteration = 0
+                # iterate through answers of that question group
+                for smaller_index, answer in enumerate(question_group):
+                    # identify which answer is taken
+                    if "[X]" in answer:
+                        answer_number = smaller_index + 1
+                        answers.append(answer_number)
+                        break
+                    else:
+                        # if an answer doesn't contain [X] then increase check count by 1
+                        checker_iteration += 1
+                # if check count was made for all answers then no answer was given therefore 0
+                if checker_iteration >= 4:
+                    answers.append(0)
+            else:
+                continue
+            
+
+        # a list of numbers of answers for each individual
+        return answers
+
+        
     # Reads the data with each line as an element in the list
     with open(collated_answers_path, "r") as coll:
         lines = coll.readlines()
@@ -94,7 +128,7 @@ def visualize_data(collated_answers_path, n):
     # for every participant as a single (smaller) list
     smaller_groups = []
     
-    # Initalize a list that is updated every 100 answers of every participant
+    # Initalize a list that is updated every 100 (or less) answers of every participant
     # That is then appended into the smaller groups
     individual_list = []
 
@@ -103,21 +137,19 @@ def visualize_data(collated_answers_path, n):
 
         # Checks if star was the first element in the file then stop
         # or else continue the implementation
-        if "*" in line:
-            if index != 0:
-                # Updates both the question group list and the participant list
-                smaller_groups.append(individual_list)
-                individual_list = []
-            else:
-                continue
+        if line.strip() == "*":
+            # Updates both the question group list and the participant list
+            smaller_groups.append(extract_answers(individual_list))
+            individual_list = []
         else:
             # Updates elements in the smaller participant list with every anwer until the (*)
-            individual_list.append(int(line.strip()))
+            individual_list.append(line)
 
     # Last check if a star was not present for the last participant
     if individual_list:
-        smaller_groups.append(individual_list)
+        smaller_groups.append(extract_answers(individual_list))
 
+    means = generate_means_sequence(collated_answers_path)
     # Plots using matplotlib.pyplot according to the input (n)
     # Scatter plot of means
     if n == 1:
@@ -128,11 +160,11 @@ def visualize_data(collated_answers_path, n):
     elif n == 2:
         # Line plots for every individual answers
         for answer in smaller_groups:
-            plt.plot(range(1, 101), answer, alpha=0.5)
+            plt.plot([i + 1 for i in range(len(answer))], answer, alpha=0.5)
         plt.xlabel("Question Number")
         plt.ylabel("Answers Value")
         plt.show()
     else:
         raise Exception("The second given input (n) should be either 1 or 2")
 
-        
+
